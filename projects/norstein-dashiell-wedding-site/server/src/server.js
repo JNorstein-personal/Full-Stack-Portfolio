289df70c6dbd1217b786164fb667536c280495af
Joshua Norstein <jsnors@gmail.com>
2026-09-19 13:54:19 -0400

    Bind wedding API to configurable host

--- projects/norstein-dashiell-wedding-site/server/src/server.js
+++ projects/norstein-dashiell-wedding-site/server/src/server.js
@@ -2,8 +2,9 @@ require("dotenv").config();
 
 const app = require("./app");
 
+const HOST = process.env.HOST || "127.0.0.1";
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Wedding website API listening on port ${PORT}`);
+app.listen(PORT, HOST, () => {
+  console.log(`Wedding website API listening on ${HOST}:${PORT}`);
 });
